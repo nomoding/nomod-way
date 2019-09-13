f95bf5957c8db5c4c7c601f4e68f445463c688a8
necheporenko <ne4eporenko.v@gmail.com>
2019-09-13 17:32:42 +0300

    fix: tweaks

--- src/components/SubscriptionForm.js
+++ src/components/SubscriptionForm.js
@@ -1,18 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SubscriptionForm = () => (
-  <form className="subscription-form">
-    <div className="input-and-btn">
-      <input type="email" name="email" placeholder="Type your email" required />
-      <button type="submit">JOIN WAITLIST</button>
-    </div>
+class SubscriptionForm extends Component {
+  state = {
+    error: false,
+    errorMessage: '',
+    success: false,
+    inputValue: ''
+  };
 
-    {false && (
-      <div className="success-message">
-        Thank you for subscribing! We’ll let you know when we go live!
-      </div>
-    )}
-  </form>
-);
+  handleInvalidInput = event => {
+    event.preventDefault();
+
+    const { inputValue } = this.state;
+    let errorMessage;
+
+    if (!inputValue) {
+      errorMessage = 'Please type your email address';
+    } else {
+      errorMessage = 'Please use a valid email address';
+    }
+
+    this.setState({ error: true, errorMessage, success: false });
+  };
+
+  handleChangeInput = event => {
+    const { value } = event.target;
+
+    this.setState({ inputValue: value });
+  };
+
+  handleSubmitForm = event => {
+    event.preventDefault();
+
+    this.setState({ error: false });
+
+    setTimeout(() => {
+      console.log('Request');
+      this.setState({ success: true, inputValue: '' });
+    }, 500);
+  };
+
+  render() {
+    const { inputValue, error, errorMessage, success } = this.state;
+    return (
+      <form className="subscription-form" onSubmit={this.handleSubmitForm}>
+        <div className="input-and-btn">
+          <input
+            type="email"
+            name="email"
+            placeholder="Type your email"
+            className={error ? 'error' : ''}
+            required
+            value={inputValue}
+            onChange={this.handleChangeInput}
+            onInvalid={e => this.handleInvalidInput(e)}
+          />
+          <button type="submit">JOIN WAITLIST</button>
+        </div>
+
+        {success && (
+          <div className="success-message">
+            Thank you for subscribing! We’ll let you know when we go live!
+          </div>
+        )}
+
+        {error && <div className="error-message">{errorMessage}</div>}
+      </form>
+    );
+  }
+}
 
 export default SubscriptionForm;
