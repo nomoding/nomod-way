44ef08c775b1e8102ec0bda6ee4a871f36db5c8b
necheporenko <ne4eporenko.v@gmail.com>
2019-09-13 18:55:33 +0300

    feat: add animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,23 +22,31 @@ const FirstScreenSection = () => (
       <div className="container">
         <div className="row">
           <div className="col-12 col-xl-7 col-lg-7 col-md-12 first-screen__text">
-            <h1>Handbook</h1>
-            <h3>
-              A guide on working at Nomod. Vivamus eget dui viverra, gravida risus ut, dictum augue.
-              Proin varius erat eget libero dignissim feugiat. Nam hendrerit, quam.
-            </h3>
+            <ScrollAnimation animateIn="fadeInLeft" animateOnce>
+              <h1>Handbook</h1>
+            </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeInLeft" animateOnce delay={150}>
+              <h3>
+                A guide on working at Nomod. Vivamus eget dui viverra, gravida risus ut, dictum
+                augue. Proin varius erat eget libero dignissim feugiat. Nam hendrerit, quam.
+              </h3>
+            </ScrollAnimation>
 
-            <form className="search-form">
-              {/* <img className="search-form__icon" src="" alt=""/> */}
-              <input type="text" placeholder="Search topics" />
-            </form>
+            <ScrollAnimation animateIn="fadeInLeft" animateOnce delay={250}>
+              <form className="search-form">
+                {/* <img className="search-form__icon" src="" alt=""/> */}
+                <input type="text" placeholder="Search topics" />
+              </form>
+            </ScrollAnimation>
           </div>
 
           <div className="col-12 col-xl-5 col-lg-5 col-md-12 first-screen__wrapper__images">
-            <div className="first-screen__images">
-              <img src={RocketImage} alt="Rocket" />
-              <img src={LuggageImage} alt="Luggage" />
-            </div>
+            <ScrollAnimation animateIn="fadeInRight" animateOnce delay={0}>
+              <div className="first-screen__images">
+                <img src={RocketImage} alt="Rocket" />
+                <img src={LuggageImage} alt="Luggage" />
+              </div>
+            </ScrollAnimation>
           </div>
         </div>
       </div>
@@ -47,13 +55,15 @@ const FirstScreenSection = () => (
   </section>
 );
 
-const Card = ({ title, description }) => (
+const Card = ({ index, title, description }) => (
   <div className="col-12 col-xl-4 col-lg-6 col-md-6">
-    <div className="card">
-      <div className="card__image"></div>
-      <div className="card__title">{title}</div>
-      <div className="card__description">{description}</div>
-    </div>
+    <ScrollAnimation animateIn="fadeIn" delay={300 + index * 50}>
+      <div className="card">
+        <div className="card__image"></div>
+        <div className="card__title">{title}</div>
+        <div className="card__description">{description}</div>
+      </div>
+    </ScrollAnimation>
   </div>
 );
 
@@ -62,12 +72,15 @@ const TopicsSection = () => (
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <div className="topics-section__title">Search by topic</div>
+          <ScrollAnimation animateIn="fadeInLeft" delay={350}>
+            <div className="topics-section__title">Search by topic</div>
+          </ScrollAnimation>
         </div>
 
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(card => (
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((card, index) => (
           <Card
             key={v4()}
+            index={index}
             title="Welcome"
             description="Cras facilisis lacus congue libero viverra, in eleifend quam accumsan. Nunc eu consectetur magna."
           />
