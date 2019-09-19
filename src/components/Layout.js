f007ad253badda5acaa662107e3cab38b2e8480e
necheporenko <ne4eporenko.v@gmail.com>
2019-09-19 17:53:52 +0300

    feat: add og: title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -51,7 +51,7 @@ const TemplateWrapper = ({ seoTitle, isAdditionalPage, children }) => {
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
+        <meta property="og:title" content={seoTitle || title} />
         <meta property="og:url" content="/" />
         <meta property="og:image" content={`${withPrefix('/')}img/og-image.jpg`} />
       </Helmet>
